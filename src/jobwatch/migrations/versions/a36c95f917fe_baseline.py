53718418ed56d99bd5dc72398a379279588f914d
Asher <asherf24@gmail.com>
2026-07-13 21:15:54 +0200

    slick alembic formatting

--- src/jobwatch/migrations/versions/a36c95f917fe_baseline.py
+++ src/jobwatch/migrations/versions/a36c95f917fe_baseline.py
@@ -6,17 +6,17 @@ Create Date: 2026-07-13 20:59:25.901638
 
 """
 
-from typing import Sequence, Union
+from collections.abc import Sequence
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy import text
 
 # revision identifiers, used by Alembic.
 revision: str = "a36c95f917fe"
-down_revision: Union[str, Sequence[str], None] = None
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+down_revision: str | Sequence[str] | None = None
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
 
 
 def upgrade() -> None:
